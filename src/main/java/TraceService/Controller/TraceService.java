ad1418b6139fa1c09b70d8ba8fa49672a1433551
Frank Gößwein <f.goesswein@mps-solutions.de>
2018-01-09 13:15:12 +0100

    cors

--- src/main/java/TraceService/Controller/TraceService.java
+++ src/main/java/TraceService/Controller/TraceService.java
@@ -10,54 +10,42 @@ public class TraceService
 {
     private TraceFiles m_TraceFiles;
 
-    private void ínitResponseHeader(HttpServletResponse p_Response)
-    {
-        p_Response.setHeader("Access-Control-Allow-Origin", "*");
-        p_Response.setHeader("Access-Control-Allow-Methods", "*");
-        p_Response.setHeader("Access-Control-Max-Age", "3600");
-        p_Response.setHeader("Access-Control-Allow-Headers", "*");
-    }
-
     public TraceService()
     {
         m_TraceFiles = new TraceFiles();
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/traceFiles")
     public TraceFiles doTraceFilesRequest(HttpServletResponse p_Response)
     {
-        ínitResponseHeader(p_Response);
-
         m_TraceFiles.loadFiles();
         return m_TraceFiles;
     }
 
     //value = "/traceFile/{id}", method = RequestMethod.GET, produces="text/xml"
+    @CrossOrigin(origins = "*")
     @GetMapping("/traceFile/{id}")
     public String doTraceFileRequest(@PathVariable("id") String p_Id, HttpServletResponse p_Response)
     {
-        ínitResponseHeader(p_Response);
-
         return m_TraceFiles.getTraceFile(p_Id);
     }
 
     //@PostMapping(path = "/members", consumes = "application/json", produces = "application/json")
+    @CrossOrigin(origins = "*")    
     @PostMapping("/newTracePath/")
     public TraceFiles doNewTracePathRequest(@RequestBody String p_Data, HttpServletResponse p_Response)
     {
-        ínitResponseHeader(p_Response);
-
         m_TraceFiles.setPath(p_Data);
         m_TraceFiles.loadFiles();
 
         return m_TraceFiles;
     }
-
+  
+    @CrossOrigin(origins = "*")
     @PostMapping("/deleteTraceFiles/")
     public TraceFiles doNewTracePathRequest(@RequestBody Integer p_Data, HttpServletResponse p_Response)
     {
-        ínitResponseHeader(p_Response);
-
         m_TraceFiles.deleteTraceFiles(p_Data);
 
         return m_TraceFiles;
